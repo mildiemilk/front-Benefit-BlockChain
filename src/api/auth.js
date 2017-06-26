d6b7d6a2395a5334b0ebddebfb784022ee41d848
CrazyBOY <thammawat_bee@hotmail.com>
2017-06-26 18:30:41 +0700

    connect to api register

--- src/api/auth.js
+++ src/api/auth.js
@@ -2,7 +2,7 @@ import { APIRequest } from '.'
 
 const LOGIN_URI = '/api/login'
 const LOGOUT_URI = 'api/logout'
-const SIGN_UP = 'api/s'
+const REGISTER_URI = 'api/register'
 
 export function authenticate(email, password) {
   return dispatch => {
@@ -15,6 +15,22 @@ export function authenticate(email, password) {
     APIRequest(options, false)
       .then(res => {
         localStorage.setItem('token', res.data.token)
+      })
+      .catch(err => console.log(err.response))
+  }
+}
+
+export function register(email, confirmPassword, password) {
+  return dispatch => {
+    const options = {
+      method: 'post',
+      url: REGISTER_URI,
+      data: { email, password, confirmPassword },
+    }
+
+    APIRequest(options, false)
+      .then(res => {
+        console.log(response)
       })
       .catch(err => console.log(err.response))
   }

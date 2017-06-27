b85d6aba6fa5800d76f6f60117834adf9eebc024
CrazyBOY <thammawat_bee@hotmail.com>
2017-06-27 17:09:35 +0700

    fix sign up and log in page

--- src/components/Auth/Signup.js
+++ src/components/Auth/Signup.js
@@ -41,17 +41,10 @@ class SignUp extends Component {
         <img src={logo} />
         <div className="boxForm">
           <h2 className="header"> สร้างบัญชีผู้ใช้ </h2>
-          <div className="row">
-            <div className="columns small-6">
-              x
-            </div>
-            <div className="columns small-6">
-              x
-            </div>
-          </div>
+          <div className="row" />
           <div className="boxCenter">
-            <Grid centered columns={3}>
-              <Grid.Column>
+            <div className="row">
+              <div className="large-4 large-offset-4 columns">
                 <Form onSubmit={this.handleSubmit}>
                   <Form.Field>
                     <Form.Input
@@ -112,8 +105,8 @@ class SignUp extends Component {
                     สมัครมสาชิก
                   </Button>
                 </Form>
-              </Grid.Column>
-            </Grid>
+              </div>
+            </div>
           </div>
         </div>
       </div>

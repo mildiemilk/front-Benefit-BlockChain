fa0bd38ff489e5a322af74469a40d655c8fa9d2a
eaoHandSumMan <en.tnc@hotmail.com>
2017-07-11 10:42:23 +0700

    upload v.2

--- src/components/uploadfile/styled.js
+++ src/components/uploadfile/styled.js
@@ -7,7 +7,7 @@ export const Detail = styled.div`
     background-color: #ffffff;
     box-shadow: 0 1px 7px 2px rgba(0, 0, 0, 0.08);
     margin-top:3%;
-    padding: 10%;
+    padding: 5%;
     padding-top: 25px;
     height: 761px;
 
@@ -19,12 +19,12 @@ export const Head = styled.p`
     font-weight: bold;
 
 `
-export const Head2 = styled.p`
-    font-size: 15px;
-  	letter-spacing: 0.4px;
-    font-weight: bold;
-    padding-top: 10px;
-
+export const TextNormal = styled.p`
+    font-family: Kanit;
+    font-size: 14px;
+    letter-spacing: 0.2px;
+    text-align: left;
+    color: #323028;
 `
 export const Inboxtext = styled.p`
     font-size: 15px;
@@ -34,55 +34,64 @@ export const Inboxtext = styled.p`
     padding-left: 5%;
     padding-top: 2%;
     text-align: center;
-
 `
 export const Inner = styled.div`
     border-radius: 8px;
     width: 100%;
-    height: 340px;
-    position:relative;
-    padding:3%;
+    height: 81px;
     border: solid 1px rgba(151, 151, 151, 0.66);
+    margin-bottom: 15px;
+    padding-top: 25px;
+    padding-left: 32px;
 `
-export const UploadBox = styled.div`
-    position:relative;
-    border: solid 1px rgba(151, 151, 151, 0.66);
+
+export const Inner2 = styled.div`
     border-radius: 8px;
     width: 100%;
-    height: 250px;
-    padding: 0px 20px 40px 20px;
-    overflow-y: scroll;
+    height: 244px;
+    border: solid 1px rgba(151, 151, 151, 0.66);
+    margin-bottom: 15px;
+    padding-top: 29px;
+    padding-left: 25px;
+    overflow: scroll;
 
 `
-export const INInner = styled.div`
-    border: dashed 1px rgba(151, 151, 151, 0.66);
-    border-radius: 8px;
-    position:relative;
-    padding:5%;
-    padding-top: 15%;
-    height: 268px;
+export const FileuploadBox = styled.div`
+
+    padding:8px;
+    width: 380px;
+    height: 40px;
+    border-radius: 5px;
+    box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.12);
+    border: solid 1px rgba(155, 155, 155, 0.64);
+
 `
-export const Submitupload = styled.button`
-    display:block;
-    margin: auto;
-    width: 268px;
-  	height: 36px;
-  	border-radius: 20px;
-  	background-color: #3a7bd5;
-    padding: 1%;
-    color: white;
-    float:center;
-    border-color: white;
+export const BrowsButton = styled.button`
+    width: 163px;
+    height: 40px;
+    border-radius: 20px;
+    color: #ffffff;
+    background-color: #3a7bd5;
+    box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.12);
+    text-align: center;
+    border-color: #3a7bd5;
+    border-style: solid;
+    color: #ffffff;
 `
+
 export const Submit = styled.button`
+    position: relative;
+    right: -700px;
+    height: 40px;
     width: 164px;
-  	height: 40px;
-  	border-radius: 20px;
-  	background-color: #f7555f;
-    padding: 1%;
-    color: white;
-    margin: 1% 80%;
-    border-color: white;
+    height: 40px;
+    border-radius: 20px;
+    color: #ffffff;
+    background-color: #f7555f;
+    box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.12);
+    text-align: center;
+    border-style: solid;
+    color: #ffffff;
 `
 
 export const inputStyle = styled.input`
@@ -102,11 +111,36 @@ export const Imagestyle = styled(Image)`
 `
 export const DropzoneStyle = styled(Dropzone)`
   &&&{
-    border: dashed 1px rgba(151, 151, 151, 0.66);
-    border-radius: 8px;
-    position:relative;
-    padding:5%;
-    padding-top: 15%;
-    height: 268px;
   }
 `
+
+// export const UploadBox = styled.div`
+//     position:relative;
+//     border: solid 1px rgba(151, 151, 151, 0.66);
+//     border-radius: 8px;
+//     width: 100%;
+//     height: 250px;
+//     padding: 0px 20px 40px 20px;
+//     overflow-y: scroll;
+//
+// `
+// export const INInner = styled.div`
+//     border: dashed 1px rgba(151, 151, 151, 0.66);
+//     border-radius: 8px;
+//     position:relative;
+//     padding:5%;
+//     padding-top: 15%;
+//     height: 268px;
+// `
+// export const Submitupload = styled.button`
+//     display:block;
+//     margin: auto;
+//     width: 268px;
+//   	height: 36px;
+//   	border-radius: 20px;
+//   	background-color: #3a7bd5;
+//     padding: 1%;
+//     color: white;
+//     float:center;
+//     border-color: white;
+// `

f7884bedd8968775166b4a9499a84d145cfca058
CrazyBOY <thammawat_bee@hotmail.com>
2017-07-22 15:20:29 +0700

    flexy plan frontend finish

--- src/components/Employee/flexy-plan/index.js
+++ src/components/Employee/flexy-plan/index.js
@@ -7,9 +7,10 @@ import { RadialChart } from 'react-vis'
 import { Responsive } from 'react-responsive'
 import gift from '../../image/gigift-mobile.png'
 import Header from '../header'
-import Footer from '../footer-absolute'
+import Footer from '../footer-relative'
 import Slider from 'react-slick'
 import FlexyPlanBox from './flexy-plan-box'
+import CongrateImage from '../../image/asset-1.png'
 import {
   Button,
   Checkbox,
@@ -39,8 +40,61 @@ class FlexyPlan extends Component {
       <div>
         <MediaQuery query="(max-width: 1224px)">
           <Header />
+
           <div className="row">
             <div className="small-10 small-centered columns">
+              <div className="deadline-flexy-plan">
+                <img src={CongrateImage} />
+                <div className="congrate-text">ยินดีด้วย!</div>
+                <div className="congrate-text">
+                  มีสิทธิประโยชน์มากมายรอคุณอยู่
+                </div>
+                <div className="deadline-box">
+                  <p>กรุณาเลือกแผนของคุณภายในวันที่ 12 เม.ย. 60</p>
+                  <div className="row">
+                    <div className="box-space">
+                      <div
+                        className="small-4 columns"
+                        style={{ paddingRight: '0px' }}
+                      >
+                        <div className="date-deadline-box">
+                          <div className="deadline-text">
+                            08
+                          </div>
+                          <div>วัน</div>
+                        </div>
+                      </div>
+                      <div
+                        className="small-4 columns"
+                        style={{ paddingRight: '0px', paddingLeft: '0px' }}
+                      >
+                        <div className="hour-deadline-box">
+                          <div className="deadline-text">
+                            08
+                          </div>
+                          <div>ชั่วโมง</div>
+                        </div>
+                      </div>
+                      <div
+                        className="small-4 columns"
+                        style={{ paddingLeft: '0px' }}
+                      >
+                        <div className="second-deadline-box">
+                          <div className="deadline-text">
+                            08
+                          </div>
+                          <div>ชั่วโมง</div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="box-space">
+                <div className="header-text-select-flexy-plan">
+                  คุณกำลังเลือก : แผน 1
+                </div>
+              </div>
               <div className="slider">
                 <Slider {...settings}>
                   <div>
@@ -51,7 +105,11 @@ class FlexyPlan extends Component {
                   </div>
                 </Slider>
               </div>
+              <button className="button-submit-flexy-plan">
+                ยืนยัน
+              </button>
             </div>
+            <Footer />
           </div>
         </MediaQuery>
       </div>

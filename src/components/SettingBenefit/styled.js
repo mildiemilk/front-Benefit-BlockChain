9d532d3faeba550fc7656477cccf10327623f289
CrazyBOY <thammawat_bee@hotmail.com>
2017-07-26 12:05:58 +0700

    fix eslink SettingBenefit

--- src/components/SettingBenefit/styled.js
+++ src/components/SettingBenefit/styled.js
@@ -168,13 +168,13 @@ export const PlanBox = styled.div`
   position: relative;
   margin-top: 18px;
   &::before {
-        content: "";
-    width: 22px;
-    height: 74px;
-    border-top-left-radius: 5px;
-    border-bottom-left-radius: 5px;
-		background: #3a7bd5;
-		position: absolute;
+      content:"";
+      width: 22px;
+      height: 74px;
+      border-top-left-radius: 5px;
+      border-bottom-left-radius: 5px;
+	    background: #3a7bd5;
+	    position: absolute;
     }
 `
 

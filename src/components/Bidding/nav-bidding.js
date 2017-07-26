04aa048bd5452950e4ecd1afaf5a58970690db12
auipw <aui_pw@hotmail.com>
2017-07-26 11:27:01 +0700

    eslint bidding

--- src/components/Bidding/nav-bidding.js
+++ src/components/Bidding/nav-bidding.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react'
 import { Divider } from 'semantic-ui-react'
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
+import CountDowns from './CountDowns'
 import {
   Head,
   Nav,
@@ -10,23 +13,21 @@ import {
   FontAucTime,
   FontNumAucTime,
 } from './styled'
-import { connect } from 'react-redux'
 import building from '../../../assets/bidding/icons-8-city.png'
 import time from '../../../assets/bidding/icons-8-time.png'
 import auction from '../../../assets/bidding/icons-8-auction.png'
-import CountDowns from './CountDowns'
 
 class Bidding extends Component {
+  static propTypes = {
+    num: PropTypes.number.isRequired,
+    timeout: PropTypes.string.isRequired,
+  }
   constructor(props) {
     super(props)
+    this.state = {}
   }
 
   render() {
-    console.log(this.props.timeout)
-    const currentDate = new Date()
-    const year = currentDate.getMonth() === 11 && currentDate.getDate() > 23
-      ? currentDate.getFullYear() + 1
-      : currentDate.getFullYear()
     return (
       <div className="NavBid">
         <div className="row">
@@ -38,7 +39,7 @@ class Bidding extends Component {
         <div className="row">
           <div className="large-4 columns">
             <Nav>
-              <Pic><img src={building} /></Pic>
+              <Pic><img src={building} alt="building" /></Pic>
               <TextNav>
                 <Font>จำนวนบริษัทประกัน</Font><br />
                 <FontNum>{this.props.num}</FontNum>
@@ -47,7 +48,7 @@ class Bidding extends Component {
           </div>
           <div className="large-4 columns">
             <Nav>
-              <Pic><img src={auction} /></Pic>
+              <Pic><img src={auction} alt="auction" /></Pic>
               <TextNav>
                 <FontAucTime>ราคาต่ำสุดในการประมูล</FontAucTime><br />
                 <FontNumAucTime>10,000,000</FontNumAucTime>
@@ -56,7 +57,7 @@ class Bidding extends Component {
           </div>
           <div className="large-4 columns">
             <Nav>
-              <Pic><img src={time} /></Pic>
+              <Pic><img src={time} alt="time" /></Pic>
               <TextNav>
                 <FontAucTime>ระยะเวลาที่เหลือในการประมูล</FontAucTime><br />
                 <FontNumAucTime>

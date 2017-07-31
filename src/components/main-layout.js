221c61e9058320dcb7613f239f9f70bd241d6ca8
CrazyBOY <thammawat_bee@hotmail.com>
2017-07-31 13:38:05 +0700

    fix frontend IPD3

--- src/components/main-layout.js
+++ src/components/main-layout.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import Sidebar from './Sidebar'
+import Sidebar from './sidebar'
 
 class MainLayout extends Component {
   static propTypes = {

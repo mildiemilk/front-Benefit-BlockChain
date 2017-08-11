dfafa0cfcb3abe723fdccbb121f03e2f2b9acc27
Freedomzzzii <theeraporn.tia@gmil.com>
2017-08-11 09:50:56 +0700

    move modal personal detail to employeeverify

---
+++ src/components/Employee/employee-verify/modal-add-data.js
@@ -0,0 +1,111 @@
+import React, { Component } from 'react'
+import { Modal, Form } from 'semantic-ui-react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+import {
+  AddDataImg,
+  Header,
+  Content,
+  SpaceHeader,
+  SpaceContent,
+  ConfirmButton,
+  SpaceImg,
+} from './styled'
+
+const ModalContents = styled(Modal.Content)`
+  &&&{
+    max-width: 320px;
+    margin: 0 auto;
+    padding-left: 4%;
+    text-align: center;
+  }
+`
+
+const Modals = styled(Modal)`
+  &&&{
+    background: transparent;
+    box-shadow: none;
+    margin-top: -120px;
+  }
+`
+class ModalAddData extends Component {
+  static propTypes = {
+    error: PropTypes.string.isRequired,
+    showModal: PropTypes.bool.isRequired,
+    handleChangeInput: PropTypes.func.isRequired,
+    handleSubmitButtonModal: PropTypes.func.isRequired,
+    handleHideModel: PropTypes.func.isRequired,
+  }
+
+  constructor(props) {
+    super(props)
+    this.state = {
+    }
+  }
+
+  render() {
+    return (
+      <Modals
+        open={this.props.showModal}
+        onClose={this.props.handleHideModel}
+      >
+        <ModalContents>
+          <div className="AddData">
+            <SpaceImg>
+              <AddDataImg src="../../../../employee/untitled-2.png" />
+            </SpaceImg>
+            <SpaceHeader>
+              <Header>
+                ยินดีต้อนรับ
+              </Header>
+            </SpaceHeader>
+            <SpaceContent>
+              <Content>
+                กรุณากรอกรายละเอียด
+                <br />
+                เพื่อสิทธิประโยชน์สูงสุดของคุณ
+              </Content>
+            </SpaceContent>
+            <Form onSubmit={this.props.handleSubmitButtonModal}>
+              <Form.Field>
+                <div className="divInput">
+                  <img
+                    className="iconLetter"
+                    src="../../../../employee/icons-8-message@3x.png"
+                    alt="letter"
+                  />
+                  <Form.Input
+                    placeholder="อีเมลของคุณ"
+                    name="email"
+                    onChange={this.props.handleChangeInput}
+                  />
+                </div>
+              </Form.Field>
+              <Form.Field>
+                <div className="divInput">
+                  <img
+                    className="iconTel"
+                    src="../../../../employee/icons-8-phone@3x.png"
+                    alt="phone"
+                  />
+                  <Form.Input
+                    placeholder="เบอร์โทรศัพท์มือถือ"
+                    name="phone"
+                    type="tel"
+                    onChange={this.props.handleChangeInput}
+                  />
+                </div>
+              </Form.Field>
+              <p style={{ color: 'red' }}>{this.props.error}</p>
+              <ConfirmButton type="submit">
+                ยืนยัน
+              </ConfirmButton>
+            </Form>
+          </div>
+        </ModalContents>
+      </Modals>
+    )
+  }
+}
+
+export default ModalAddData
---
+++ src/reducers/user.js
@@ -0,0 +1,45 @@
+/**
+ * Default State
+ */
+const defaultUser = {
+  user: null,
+  personalEmail: '',
+  phone: '',
+  message: '',
+  error: false,
+}
+
+/**
+ * Action Constansts
+ */
+const UPDATE_PERSONALDETAIL_SUCCESS = 'user/UPDATE_PERSONALDETAIL_SUCCESS'
+const UPDATE_PERSONALDETAIL_FAILURE = 'user/UPDATE_PERSONALDETAIL_FAILURE'
+
+/**
+ * Actions
+ */
+export function updatePersonalDetailSuccess(data) {
+  return { type: UPDATE_PERSONALDETAIL_SUCCESS, data }
+}
+
+export function updatePersonalDetailFailure(data) {
+  return { type: UPDATE_PERSONALDETAIL_FAILURE, data }
+}
+
+export function userReducer(state = defaultUser, action) {
+  switch (action.type) {
+    case UPDATE_PERSONALDETAIL_SUCCESS:
+      return Object.assign({}, state, {
+        personalEmail: action.data.personalEmail,
+        phone: action.data.phone,
+        error: false,
+      })
+    case UPDATE_PERSONALDETAIL_FAILURE:
+      return Object.assign({}, state, {
+        message: action.data.error,
+        error: true,
+      })
+    default:
+      return state
+  }
+}

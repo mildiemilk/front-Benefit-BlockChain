4f77d109965ad9ed578320cffbceef21a8d62082
Pumkinprimo <prim281260@gmail.com>
2017-09-06 14:15:49 +0700

    Auto stash before merge of "master" and "origin/master"

--- src/components/appinsurer.js
+++ src/components/appinsurer.js
@@ -8,23 +8,21 @@ import '../styles/InsurerStyle/main.scss';
 import Header from './Insurer/Header';
 import Welcome from './Insurer/welcome';
 import Dashboard from './Insurer/dashboard-insurer';
-import BiddingList from './Insurer/Bidding';
+import Bidding from './Insurer/Bidding';
+import BiddingList from './Insurer/bidding-list';
 import MainLayout from './Insurer/main-layout';
 import Logout from './Auth/logout';
-import InsurerLogin from './Insurer/insurer-login';
-
 const AppInsurer = () => (
   <BrowserRouter>
     <div style={{ height: '100%' }}>
       <Header />
       <Switch>
-        <Route path="/logout" component={Logout} />
-        <Route path="/insurerlogin" component={InsurerLogin} />
         <Route path="/dashboard" component={Dashboard} />
         <Route path="/welcome" component={Welcome} />
         <MainLayout>
           <Switch>
             <Route path="/biddinglist" component={BiddingList} />
+            <Route path="/bidding" component={Bidding} />
           </Switch>
         </MainLayout>
         {/* <Route path="/biddinglist" component={BiddingList} />

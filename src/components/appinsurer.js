18863ce0ab1d3d36d354ad603943c1cd018316c8
Pumkinprimo <prim281260@gmail.com>
2017-09-06 12:11:57 +0700

    add appInsurer

--- src/components/appinsurer.js
+++ src/components/appinsurer.js
@@ -4,26 +4,17 @@ import { connect } from 'react-redux';
 import { Switch, Route } from 'react-router';
 import { BrowserRouter } from 'react-router-dom';
 import 'semantic-ui-css/semantic.min.css';
-import '../styles/employee-style/main.scss';
-// import Header from './Insurer/Header';
+import '../styles/InsurerStyle/main.scss';
+import Header from './Insurer/Header';
 import Welcome from './Insurer/welcome';
 import Dashboard from './Insurer/dashboard-insurer';
-import Bidding from './Insurer/Bidding';
+import BiddingList from './Insurer/Bidding';
 import MainLayout from './Insurer/main-layout';
-<<<<<<< Updated upstream
-import InsurerLogin from './Insurer/insurer-login';
-=======
-import NavLayout from './nav-layout';
-import Logout from './Auth/logout';
->>>>>>> Stashed changes
-
 const AppInsurer = () => (
   <BrowserRouter>
     <div style={{ height: '100%' }}>
-<<<<<<< Updated upstream
       <Header />
       <Switch>
-        <Route path="/insurerlogin" component={InsurerLogin} />
         <Route path="/dashboard" component={Dashboard} />
         <Route path="/welcome" component={Welcome} />
         <MainLayout>
@@ -36,37 +27,14 @@ const AppInsurer = () => (
         <Route path="/companyclaimlist" component={CompanyClaimList} />
         <Route path="/viewclaim" component={ViewClaimList} /> */}
       </Switch>
-=======
-      <NavLayout>
-        <Switch>
-          <Route path="/dashboard" component={Dashboard} />
-          <Route path="/logout" component={Logout} />
-          <Route path="/congrat" component={Welcome} />
-          <MainLayout>
-            <Switch>
-              <Route path="/ibidding" component={Bidding} />
-
-            </Switch>
-          </MainLayout>
-          {/* <Route path="/biddinglist" component={BiddingList} />
-          <Route path="/bidding" component={Bidding} />
-          <Route path="/companyclaimlist" component={CompanyClaimList} />
-          <Route path="/viewclaim" component={ViewClaimList} /> */}
-        </Switch>
-      </NavLayout>
->>>>>>> Stashed changes
     </div>
   </BrowserRouter>
-
 );
-
 AppInsurer.propTypes = {
   isAuthenticated: PropTypes.bool.isRequired,
 };
-
 const mapStateToProps = state => ({
   isAuthenticated: state.authReducer.token != null,
 });
-
 const Container = connect(mapStateToProps)(AppInsurer);
 export default Container;

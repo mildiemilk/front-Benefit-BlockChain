2d6e3ad3aac7608fbb0a5654cf5b899405cd0c92
Pumkinprimo <prim281260@gmail.com>
2017-09-08 20:25:42 +0700

    Add icon-main css

--- src/components/Insurer/Claim/styled.js
+++ src/components/Insurer/Claim/styled.js
@@ -17,7 +17,7 @@ export const TextIn = styled.div`
 `;
 
 export const Nav = styled.div`
-  height: 130px;
+  height: 200px;
   border-radius: 6px;
   background-color: #ffffff;
   box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.12);

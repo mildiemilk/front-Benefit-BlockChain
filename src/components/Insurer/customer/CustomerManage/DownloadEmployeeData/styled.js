98cf9a43a74100cd3a5d76583e1734af4a60a8d5
eaohandsome <en.tnc@hotmail.com>
2017-09-28 18:30:27 +0700

    add cumtomer manage stap2

---
+++ src/components/Insurer/customer/CustomerManage/DownloadEmployeeData/styled.js
@@ -0,0 +1,62 @@
+import styled from 'styled-components';
+
+export const CustomerName = styled.div`
+  margin: 0px 20px;
+  display: inline-block;
+  font-size: 16px;
+  text-align: left;
+`;
+
+export const WhiteBackGround = styled.div`
+  background-color: white;
+  width: 100%;
+  height: 100%;
+  border-radius: 5px;
+  box-shadow: 0 1px 7px 2px rgba(0, 0, 0, 0.08);
+  padding: 50px;
+`;
+
+export const HeadSecondDiv = styled.div`
+  font-size: 20px;
+  font-weight: 500;
+  letter-spacing: 0.3px;
+  padding: 15px 0px;
+  text-align: left;
+`;
+
+export const NextButton = styled.div`
+  width: 164px;
+  height: 40px;
+  border-radius: 20px;
+  background-color: #46b3b8;
+  text-align: center;
+  float: right;
+  color: #ffffff;
+  font-size: 20px;
+  padding: 12px;
+  margin: 30px;
+  cursor: pointer;
+`;
+
+export const BackButton = styled.div`
+  width: 164px;
+  height: 40px;
+  border-radius: 20px;
+  background-color: #f7555f;
+  text-align: center;
+  float: left;
+  color: #ffffff;
+  font-size: 20px;
+  padding: 12px;
+  margin: 30px;
+  cursor: pointer;
+`;
+
+export const DownloadDiv = styled.div`
+  width: 100%;
+  height: 100px;
+  padding: 30px;
+  border-radius: 8px;
+  background-color: #ffffff;
+  box-shadow: 0 1px 5px 0 rgba(0, 0, 0, 0.2);
+`;
--- src/components/Insurer/customer/CustomerManage/ViewPlan/ModalPlanBox/styled.js
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components';
-
-export const ListBox = styled.div`
-    float: left !important;
-    margin-bottom: 2%;
-`;
